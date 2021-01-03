cc0b2dd7fa182847cb25742350d40c5d9d08d4b1
Jose Pedro <josepedro88@live.com.pt>
2021-01-03 15:18:00 +0000

    API popular games URL

---
+++ src/api.js
@@ -0,0 +1,35 @@
+//GET DATE
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+const currentYear = new Date().getFullYear();
+
+//CURRENT day/month/year
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+
+//OTHER DATES
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// BASE URL
+const base_url = "https://api.rawg.io/api/";
+
+//POPULAR GAMES
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
