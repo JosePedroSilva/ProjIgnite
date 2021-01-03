b01e0a2039c5624b7aaab16cd5fc21ba530bbeb1
Jose Pedro <josepedro88@live.com.pt>
2021-01-03 19:33:06 +0000

    details and screenshots fetched

--- src/api.js
+++ src/api.js
@@ -37,3 +37,8 @@ const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${newGames}`;
+
+//GAME DETAILS
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots`;
